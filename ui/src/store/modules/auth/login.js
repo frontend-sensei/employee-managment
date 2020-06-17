a8324314c64d60ccb1cf14f7520948fc602d3a3e
Jo Ker <frontendsensei612@gmail.com>
2020-06-18 01:53:07 +0300

    modal actions, mutations and states added

--- ui/src/store/modules/auth/login.js
+++ ui/src/store/modules/auth/login.js
@@ -1,3 +1,31 @@
+import axios from 'axios'
+import router from '@/app-routes'
+
 export default {
-  
+  actions: {
+
+    authLogin({}, data) {
+      axios({
+        method: 'post',
+        url: '/login',
+        headers: {'Content-Type': 'application/json'},
+        data: JSON.stringify(data)
+      })
+      .then((res) => {
+        const token = res.data.token
+        localStorage.setItem('token', token)
+        router.push('/')
+      })
+      .catch(err => {
+        localStorage.removeItem('token')
+        console.log('error', err)
+      })
+    },
+
+    authLogout() {
+      localStorage.removeItem('token')
+      router.push('/auth')
+    }
+
+  }
 }
