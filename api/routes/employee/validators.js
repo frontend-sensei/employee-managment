36e2b1a05ca8d0c29c03848266eab56a40cc0a5f
Jo Ker <frontendsensei612@gmail.com>
2020-06-18 14:05:54 +0300

    employee CRUD api routes and validators added

--- api/routes/employee/validators.js
+++ api/routes/employee/validators.js
@@ -22,33 +22,3 @@ exports.employeeValidators = [
     .isNumeric()
     .withMessage('Salary must be numeric'),
 ]
-
-// exports.loginValidators = [  
-//   body('password')
-//     .isLength({min: 3, max: 20})
-//     .withMessage('Password length must be at least 3 characters')
-//     .custom(async (value, {req}) => {
-
-//       try {
-//         const login = req.body.login
-//         const candidate = await User.findOne({ login })
-
-//         if (candidate) {
-//           // User with same login exists
-//           const areSame = await bcrypt.compare(value, candidate.password)
-
-//           if (areSame) {
-//             return true
-//           } else {
-//             return Promise.reject('Wrong password.')
-//           }
-//         } else {
-//           return Promise.reject('User with same login doesn`t exist.')
-//         }
-
-//       } catch (e) {
-//         console.log(e)
-//       }
-
-//     })
-// ]
