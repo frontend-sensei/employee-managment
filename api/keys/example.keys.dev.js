c009c1889c347bf585c23d484affbc950a10457a
Jo Ker <frontendsensei612@gmail.com>
2020-06-19 21:44:40 +0300

    add example keys.dev.js

---
+++ api/keys/example.keys.dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB_URI: 'mongodb+srv://<your_user_name>:<your_password>@<your_cluster>.mongodb.net/<your_dbname>',
+  SESSION_SECRET: 'example secret value',
+  BASE_URL: 'http://localhost:3000',
+  JWT_SECRET: 'example_secret'
+}
